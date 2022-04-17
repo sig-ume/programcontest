e99f8724d0cf98c399686ffda348c157b678d3ec
sig-ume <beyondthemoonface@gmail.com>
2022-04-17 22:18:49 +0900

    Mainクラスのテンプレにしゃくとり法メソッドを追加
    - DPとかテンプレ化できるやつは追加しときたい

--- src/main/java/jp/sigre/contest/Main.java
+++ src/main/java/jp/sigre/contest/Main.java
@@ -1,5 +1,7 @@
 package jp.sigre.contest;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class Main {
@@ -9,6 +11,33 @@ public class Main {
 
         int n = sc.nextInt();
     }
+
+    /**
+     * しゃくとり法
+     * @param ints
+     * @return
+     */
+    private static long shakutori(int[] ints) {
+        long result = 0;
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < ints.length; i++) {
+            int num = ints[i];
+
+            deque.add(num);
+            int left = 0;
+            while (left == 0) {
+
+                if (deque.peekLast() == null) {
+                    break;
+                }
+                left = deque.pollFirst();
+            }
+        }
+
+        return result;
+    }
+
 
     private static int[] getMultipleNumbers(Scanner sc, int count) {
         int[] nums = new int[count];
