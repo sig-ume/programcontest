a02e1c51d8f62e4e8aab637c154c60e2aa3bb081
sig-ume <beyondthemoonface@gmail.com>
2022-04-24 15:12:51 +0900

    https://atcoder.jp/contests/abc249 ABC D問題回答
    - resultが数値の場合longにしよう

--- src/main/java/jp/sigre/contest/abc/abc249/d/Main.java
+++ src/main/java/jp/sigre/contest/abc/abc249/d/Main.java
@@ -2,7 +2,6 @@ package jp.sigre.contest.abc.abc249.d;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -12,37 +11,27 @@ public class Main {
 
         int n = sc.nextInt();
 
-        int[] nums = getMultipleNumbers(sc, n);
-        Map<Trips, Boolean> memo = new HashMap<>();
+        int[] a = getMultipleNumbers(sc, n);
 
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (check(nums, i, j, k, memo)) {
-                       result++;
-                    }
-                }
+        Map<Integer, Integer> c = new HashMap<>();
+        int aMax = 0;
+        for (int i : a) {
+            c.merge(i, 1, Integer::sum);
+            aMax = Math.max(aMax, i);
+        }
+
+        long result = 0;
+        for (int aj = 1; aj <= aMax; aj++) {
+            for (int ak = 1; ak <= aMax / aj; ak++) {
+                if (aj * ak > aMax) continue;
+
+                result += (long) c.getOrDefault(aj, 0)
+                        * c.getOrDefault(ak, 0)
+                        * c.getOrDefault(aj * ak, 0);
             }
         }
 
         System.out.println(result);
-    }
-    
-    private static boolean check(int[]nums , int i, int j, int k, Map<Trips, Boolean> memo) {
-        int ai = nums[i];
-        int aj = nums[j];
-        int ak = nums[k];
-
-        Trips trips = new Trips(ai, aj, ak);
-
-        if (memo.containsKey(trips)) return memo.get(trips);
-        if (ai%aj != 0) {
-            memo.put(trips, false);
-            return false;
-        }
-        memo.put(trips, ai / aj == ak);
-        return ai / aj == ak;
     }
 
     private static int[] getMultipleNumbers(Scanner sc, int count) {
@@ -53,30 +42,5 @@ public class Main {
         }
 
         return nums;
-    }
-
-    private static class Trips {
-        int i;
-        int j;
-        int k;
-
-        public Trips(int i, int j, int k) {
-            this.i = i;
-            this.j = j;
-            this.k = k;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Trips trips = (Trips) o;
-            return i == trips.i && j == trips.j && k == trips.k;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(i, j, k);
-        }
     }
 }
