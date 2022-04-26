8cf6e8566eaf500a6f73ece505f7179ea5586f0d
sig-ume <beyondthemoonface@gmail.com>
2022-04-26 23:50:21 +0900

    https://atcoder.jp/contests/arc139 A問題回答
    - やっとだよ！！！
    - Tの値が増える・そのまま・減る、の3パターンで分岐させるつもりが、最後に作った増えるパターンが他の2パターンに適用可能なことに気づき、コードが1/3に
    - 値を左シフト→1 or 2を足す→値を右シフト だけでいいはずが、なぜか「小さい桁から見て最初の0を1に変更する」必要があると謎の勘違い。気づくのに2日かかった

--- src/main/java/jp/sigre/contest/arc/arc139/a/Main.java
+++ src/main/java/jp/sigre/contest/arc/arc139/a/Main.java
@@ -8,41 +8,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] ts = getMultipleNumbers(sc, n);
-        String s3 = Long.toBinaryString(219901226043572224L);
 
         long now = 0;
-        int pastT = -1;
-        boolean first = true;
         for (int t : ts) {
-            if (first) {
-                now = 1L << t;
-                first = false;
-            } else if (pastT > t) {
-                now = now | 1L << t;
-            } else if (pastT == t) {
-                now = now + (1L << (t+1));
-//                String s = Long.toBinaryString(now);
-//                String s1 = s.substring(0, s.length() - t);
-//                int last0 = s1.lastIndexOf("0");
-//                if (last0 == -1) {
-//                    String s2 = Long.toBinaryString(now);
-//                    now = 1L << t;
-//                    if (s2.length() > t) now += 1L << s2.length();
-//                    continue;
-//                } else now = now | 1L << (s.length() - last0 - t);
+            long tmp = now >> t;
+            if (Long.numberOfTrailingZeros(tmp) > 0) {
+                tmp++;
             } else {
-                String s = Long.toBinaryString(now);
-                now = 1L << t;
-                if (s.length() > t) now += 1L << s.length();
+                tmp += 2;
             }
+            now = tmp << t;
 
-            pastT = t;
         }
 
         System.out.println(now);
-        //System.out.println(Long.toBinaryString(now));
     }
-
 
     private static int[] getMultipleNumbers(Scanner sc, int count) {
         int[] nums = new int[count];
