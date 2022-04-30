ec9d823377fe2a8a99a0d55805fb4c560cdd7abb
sig-ume <beyondthemoonface@gmail.com>
2022-04-30 15:18:07 +0900

    https://atcoder.jp/contests/typical-algorithm C回答
    - bitDPで、スタートの地点は通過したノードの集合には含めない。なので初期状態のノード集合は0

---
+++ src/main/java/jp/sigre/contest/typical_algorithm/c/Main.java
@@ -0,0 +1,49 @@
+package jp.sigre.contest.typical_algorithm.c;
+
+import java.util.Scanner;
+
+public class Main {
+    static long[][] memo;
+    static long[][] roads;
+    static int n = 0;
+    private static final long INF = 13000000000L;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        roads = new long[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                roads[i][j] = sc.nextLong();
+            }
+        }
+
+        memo = new long[n][1 << n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 1 << n; j++) {
+                memo[i][j] = INF;
+            }
+        }
+        memo[0][0] = 0;
+
+        int allDone = (1 << n) - 1;
+
+        System.out.println(calc(0, allDone));
+    }
+
+    private static long calc(int now, int s) {
+        if (memo[now][s] != INF) return memo[now][s];
+
+        if (now == 0 && s != 0 && s != (1 << n) - 1) return INF;
+        if (((1 << now) & s) == 0) return INF;
+
+        long min = INF;
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, calc(i, s ^ (1 << now)) + roads[i][now]);
+        }
+
+        return memo[now][s] = min;
+    }
+}
