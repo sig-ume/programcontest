90e10b687c229c65e6ee0167a37e3484277abf07
sig-ume <beyondthemoonface@gmail.com>
2022-05-06 03:19:57 +0900

    https://atcoder.jp/contests/abc248 D回答
    - Javaの標準入出力が貧弱、という悲しい事実を学びました
    - TODO:テンプレに自作標準入出力を組み込む
    - その他、解き直しリスト(仮)を作成

--- src/main/java/jp/sigre/contest/abc/abc248/d/Main.java
+++ src/main/java/jp/sigre/contest/abc/abc248/d/Main.java
@@ -1,79 +1,67 @@
 package jp.sigre.contest.abc.abc248.d;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.*;
 
 public class Main {
 
-    static long INF = 1000000000000L;
-    static long res = 0;
     static int n;
-    private static long[] bp;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        FastScanner sc = new FastScanner();
 
         n = sc.nextInt();
         int[] as = getMultipleNumbers(sc, n);
 
-        // RとX
-        bp = new long[n + 1];
-
         int q = sc.nextInt();
 
-        Qu[] qs = new Qu[q];
+        Map<Integer, List<List<Integer>>> qMap = new HashMap<>(n + 1, 2);
+
         for (int i = 0; i < q; i++) {
-            qs[i] = new Qu(i, sc.nextInt(), sc.nextInt(), sc.nextInt());
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            int x = sc.nextInt();
+            addXAndI(qMap, i, l - 1, x, 0);
+            addXAndI(qMap, i, r, x, 1);
         }
 
-        Arrays.sort(qs);
+        long[] res = new long[q];
+        long[] cnt = new long[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int ai = as[i];
+            cnt[ai]++;
 
-        int bef = 0;
-        for (Qu qu : qs) {
-            if (bef != qu.x) {
-// bp更新
-                bp = new long[n + 1];
-                for (int r = 1; r <= n; r++) {
-                    bp[r] = bp[r - 1];
-                    int ar = as[r];
-                    if (ar == qu.x) {
-                        bp[r]++;
-                    }
+            if (!qMap.containsKey(i)) continue;
+
+            List<List<Integer>> queues = qMap.get(i);
+            for (List<Integer> qu : queues) {
+                Integer x = qu.get(0);
+                Integer idx = qu.get(1);
+                Integer isR = qu.get(2);
+                if (isR == 1) {
+                    res[idx] += cnt[x];
+                } else {
+                    res[idx] -= cnt[x];
                 }
-                bef = qu.x;
             }
-            qu.c = bp[qu.r] - bp[qu.l - 1];
         }
 
-        Arrays.sort(qs, Comparator.comparingInt(o -> o.i));
-
-        for (Qu qu : qs) {
-            System.out.println(qu.c);
+        PrintWriter out = new PrintWriter(System.out);
+        for (long r : res) {
+            out.println(r);
         }
+        out.flush();
     }
 
-    private static class Qu implements Comparable<Qu> {
-        int i;
-        int l;
-        int r;
-        int x;
-        long c;
-
-        public Qu(int i, int l, int r, int x) {
-            this.i = i;
-            this.l = l;
-            this.r = r;
-            this.x = x;
-        }
-
-        @Override
-        public int compareTo(Qu o) {
-            return Integer.compare(x, o.x);
-        }
+    private static void addXAndI(Map<Integer, List<List<Integer>>> qMap, int i, int lr, int x, int isR) {
+        List<List<Integer>> orDefault = qMap.getOrDefault(lr, new ArrayList<>());
+        orDefault.add(Arrays.asList(x, i, isR));
+        qMap.put(lr, orDefault);
     }
 
-    private static int[] getMultipleNumbers(Scanner sc, int count) {
+    private static int[] getMultipleNumbers(FastScanner sc, int count) {
         int[] nums = new int[count + 1];
         nums[0] = 0;
         for (int i = 1; i <= count; i++) {
@@ -81,5 +69,88 @@ public class Main {
         }
 
         return nums;
+    }
+
+    @SuppressWarnings("unused")
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1024];
+        private int ptr = 0;
+        private int bufLen = 0;
+
+        private boolean hasNextByte() {
+            if (ptr < bufLen) {
+                return true;
+            } else {
+                ptr = 0;
+                try {
+                    bufLen = in.read(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return bufLen > 0;
+            }
+        }
+
+        private int readByte() {
+            if (hasNextByte()) return buffer[ptr++];
+            else return -1;
+        }
+
+        private static boolean isPrintableChar(int c) {
+            return 33 <= c && c <= 126;
+        }
+
+        @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+        public boolean hasNext() {
+            while (hasNextByte() && !isPrintableChar(buffer[ptr])) ptr++;
+            return hasNextByte();
+        }
+
+        public String next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            StringBuilder sb = new StringBuilder();
+            int b = readByte();
+            while (isPrintableChar(b)) {
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public long nextLong() {
+            if (!hasNext()) throw new NoSuchElementException();
+            long n = 0;
+            boolean minus = false;
+            int b = readByte();
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+            if (b < '0' || '9' < b) {
+                throw new NumberFormatException();
+            }
+            while (true) {
+                if ('0' <= b && b <= '9') {
+                    n *= 10;
+                    n += b - '0';
+                } else if (b == -1 || !isPrintableChar(b)) {
+                    return minus ? -n : n;
+                } else {
+                    throw new NumberFormatException();
+                }
+                b = readByte();
+            }
+        }
+
+        public int nextInt() {
+            long nl = nextLong();
+            if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) throw new NumberFormatException();
+            return (int) nl;
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
     }
 }
