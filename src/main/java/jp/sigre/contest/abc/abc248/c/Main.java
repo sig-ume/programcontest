a82becd450f6948dd9c0c98102496a9075c1eccf
sig-ume <beyondthemoonface@gmail.com>
2022-05-13 00:47:15 +0900

    https://atcoder.jp/contests/abc248 C再回答

--- src/main/java/jp/sigre/contest/abc/abc248/c/Main.java
+++ src/main/java/jp/sigre/contest/abc/abc248/c/Main.java
@@ -3,24 +3,51 @@ package jp.sigre.contest.abc.abc248.c;
 import java.util.Scanner;
 
 public class Main {
+    private static class C {
+        long res;
+        int N;
+        int M;
+        int K;
+        long[][] memo;
+        int border = 998244353;
 
-    static long INF = 1000000000000L;
-    static long res = 0;
-    static int n;
+        public long m() {
+            Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+            N = sc.nextInt();
+            M = sc.nextInt();
+            K = sc.nextInt();
 
-        n = sc.nextInt();
-    }
+            memo = new long[N + 1][K + 1];
+            memo[0][0] = 1;
 
-    private static int[] getMultipleNumbers(Scanner sc, int count) {
-        int[] nums = new int[count];
+            for (int i = 0; i <= N; i++) {
+                for (int j = 0; j <= K; j++) {
+                    dp(i, j);
+                }
+            }
 
-        for (int i = 0; i < count; i++) {
-            nums[i] = sc.nextInt();
+            for (int i = 0; i <= K; i++) {
+                res += memo[N][i];
+                if (res >= border) res -= border;
+            }
+
+            return res;
         }
 
-        return nums;
+        private void dp(int i, int sum) {
+            for (int j = 1; j <= M; j++) {
+                if (i + 1 <= N && sum + j <= K) {
+                    memo[i + 1][sum + j] += memo[i][sum];
+                    if (memo[i + 1][sum + j] >= border) {
+                        memo[i + 1][sum + j] -= border;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new C().m());
     }
 }
