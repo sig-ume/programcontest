f2c8e44ade8e3aef3910d82f8c2e85b1401e1c15
sig-ume <beyondthemoonface@gmail.com>
2022-05-14 23:57:52 +0900

    https://atcoder.jp/contests/abc251 ABC+D回答
    - Dが完全に気付くかどうかゲー。進数つかってやるかってとこまでは考えたけどまさか100進数とは…
    - DP問題がよく出るので対策しようね

---
+++ src/main/java/jp/sigre/contest/abc/abc251/c/Main.java
@@ -0,0 +1,126 @@
+package jp.sigre.contest.abc.abc251.c;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Main {
+
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        String[] ss = new String[n];
+        int[] ts = new int[n];
+        for (int i = 0; i < n; i++) {
+            ss[i] = sc.next();
+            ts[i] = sc.nextInt();
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        int idx = -1;
+        int maxP = 0;
+        for (int i = 0; i < n; i++) {
+            String s = ss[i];
+            if (map.containsKey(s)) {
+                continue;
+            }
+            map.put(s, 0);
+            if (ts[i] > maxP) {
+                idx = i;
+                maxP = ts[i];
+            }
+        }
+
+        System.out.println(idx + 1);
+    }
+
+    @SuppressWarnings("unused")
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1024];
+        private int ptr = 0;
+        private int bufLen = 0;
+
+        private boolean hasNextByte() {
+            if (ptr < bufLen) {
+                return true;
+            } else {
+                ptr = 0;
+                try {
+                    bufLen = in.read(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return bufLen > 0;
+            }
+        }
+
+        private int readByte() {
+            if (hasNextByte()) return buffer[ptr++];
+            else return -1;
+        }
+
+        private static boolean isPrintableChar(int c) {
+            return 33 <= c && c <= 126;
+        }
+
+        @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+        public boolean hasNext() {
+            while (hasNextByte() && !isPrintableChar(buffer[ptr])) ptr++;
+            return hasNextByte();
+        }
+
+        public String next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            StringBuilder sb = new StringBuilder();
+            int b = readByte();
+            while (isPrintableChar(b)) {
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public long nextLong() {
+            if (!hasNext()) throw new NoSuchElementException();
+            long n = 0;
+            boolean minus = false;
+            int b = readByte();
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+            if (b < '0' || '9' < b) {
+                throw new NumberFormatException();
+            }
+            while (true) {
+                if ('0' <= b && b <= '9') {
+                    n *= 10;
+                    n += b - '0';
+                } else if (b == -1 || !isPrintableChar(b)) {
+                    return minus ? -n : n;
+                } else {
+                    throw new NumberFormatException();
+                }
+                b = readByte();
+            }
+        }
+
+        public int nextInt() {
+            long nl = nextLong();
+            if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) throw new NumberFormatException();
+            return (int) nl;
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
