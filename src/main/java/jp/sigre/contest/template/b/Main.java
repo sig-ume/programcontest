9f4f6417db02b0473ad21af51fba22986b4b1919
sig-ume <beyondthemoonface@gmail.com>
2022-05-25 22:56:21 +0900

    https://atcoder.jp/contests/abc250 C再回答
    - あわせてテンプレ微修正

--- src/main/java/jp/sigre/contest/template/b/Main.java
+++ src/main/java/jp/sigre/contest/template/b/Main.java
@@ -7,17 +7,16 @@ import java.util.Scanner;
 
 public class Main {
 
-    static long INF = 1000000000000L;
-    static long res = 0;
-    static int n;
-
+    static int N;
+    static int tmp;
+    static Scanner sc;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
 
-        n = sc.nextInt();
+        N = sc.nextInt();
     }
 
-    private static int[] getMultipleNumbers(Scanner sc, int count) {
+    private static int[] getMultipleNumbers(int count) {
         int[] nums = new int[count];
 
         for (int i = 0; i < count; i++) {
